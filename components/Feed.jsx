9b0648ba42fee65cf628fb02ed422b54772e4355
Phil Smithies <philsmithies@gmail.com>
2021-11-01 04:23:45 +0100

    updated nav links and index tidy up

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,8 +7,6 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function Feed() {
   const { data, error } = useSwr("/api/tweets", fetcher);
 
-  console.log(data);
-
   if (error) return <div>Failed to load Tweets</div>;
   if (!data) return <div>Loading...</div>;
 
