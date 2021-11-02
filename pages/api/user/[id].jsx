c198f52884e83e0ebef16d8855874697acc4b328
Phil Smithies <philsmithies@gmail.com>
2021-11-02 02:03:10 +0100

    profile api endpoints

--- pages/api/user/[id].jsx
+++ pages/api/user/[id].jsx
@@ -7,12 +7,4 @@ dbConnect();
 export default async (req, res) => {
   const user = await User.find({ _id: ObjectId(req.query.id) });
   res.json(user);
-
-  // getAllTweets: async (req, res) => {
-  //   const reqUser = await UserModel.findOne({ username: req.params.username });
-  //   let foundUser = await TweetModel.find({ author: reqUser._id })
-  //     .populate("author")
-  //     .sort({ createdAt: "desc" });
-  //   res.json(foundUser);
-  // },
 };
