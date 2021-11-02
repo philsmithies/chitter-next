62865a86f508490721af402ec59d3f97727fa49d
Phil Smithies <philsmithies@gmail.com>
2021-11-02 05:46:07 +0100

    loading bar for image uploads

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,6 +1,7 @@
 import { signIn } from "next-auth/client";
 import React, { useRef, useState } from "react";
 import Axios from "axios";
+import PuffLoader from "react-spinners/PuffLoader";
 
 const SignUp = () => {
   const url = "https://api.cloudinary.com/v1_1/dryaxqxie/image/upload";
@@ -11,6 +12,7 @@ const SignUp = () => {
   const fullNameRef = useRef(null);
   const [file, setFile] = useState("");
   const [image, setImage] = useState("no image");
+  const [loading, setLoading] = useState(false);
 
   const onChange = async (e) => {
     setFile(e.target.files[0]);
@@ -34,6 +36,7 @@ const SignUp = () => {
       return;
     }
     try {
+      setLoading(true);
       await Axios.post(
         "/api/users",
         {
@@ -47,7 +50,11 @@ const SignUp = () => {
           withCredentials: true,
         }
       ).then((response) => {
+        setLoading(false);
         console.log(response);
+        if (response.data.success) {
+          signIn();
+        }
       });
     } catch (err) {
       console.error(err);
@@ -70,44 +77,60 @@ const SignUp = () => {
   };
 
   return (
-    <div className="bg-gray-800 h-screen flex items-center justify-center">
-      <form
-        className="bg-white w-3/12 flex flex-col mb-56 p-3 rounded"
-        onSubmit={onFormSubmit}
-      >
-        <input
-          className="border-2 rounded-md p-1"
-          ref={emailRef}
-          placeholder="email"
-        />
-        <br />
-        <input
-          className="border-2 rounded-md p-1"
-          ref={usernameRef}
-          placeholder="username"
-        />
-        <br />
-        <input
-          className="border-2 rounded-md p-1"
-          ref={fullNameRef}
-          placeholder="full name"
-        />
-        <br />
-        <input
-          className="border-2 rounded-md p-1"
-          ref={passwordRef}
-          placeholder="password"
-        />
-        <br />
-        <input
-          className="border-2 p-1 mb-2"
-          accept="image/*"
-          multiple
-          type="file"
-          onChange={onChange}
-        />
-        <button className="border-2 mt-3 bg-yellow-400 p-2">Submit</button>
-      </form>
+    <div
+      className={`bg-gray-800 h-screen flex ${
+        !loading ? "items-center" : ""
+      } justify-center`}
+    >
+      {loading && (
+        <div className="mt-36">
+          <PuffLoader
+            color={"#36D7B7"}
+            // loading={loading}
+            // css={override}
+            size={100}
+          />
+        </div>
+      )}
+      {!loading && (
+        <form
+          className="bg-white w-3/12 flex flex-col mb-56 p-3 rounded"
+          onSubmit={onFormSubmit}
+        >
+          <input
+            className="border-2 rounded-md p-1"
+            ref={emailRef}
+            placeholder="email"
+          />
+          <br />
+          <input
+            className="border-2 rounded-md p-1"
+            ref={usernameRef}
+            placeholder="username"
+          />
+          <br />
+          <input
+            className="border-2 rounded-md p-1"
+            ref={fullNameRef}
+            placeholder="full name"
+          />
+          <br />
+          <input
+            className="border-2 rounded-md p-1"
+            ref={passwordRef}
+            placeholder="password"
+          />
+          <br />
+          <input
+            className="border-2 p-1 mb-2"
+            accept="image/*"
+            multiple
+            type="file"
+            onChange={onChange}
+          />
+          <button className="border-2 mt-3 bg-yellow-400 p-2">Submit</button>
+        </form>
+      )}
     </div>
   );
 };
