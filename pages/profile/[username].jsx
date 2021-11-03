0f259608bb4fe0e350c94794241e7ddc5573348e
Phil Smithies <philsmithies@gmail.com>
2021-11-03 04:20:15 +0100

    tweet content availiable on profile page

--- pages/profile/[username].jsx
+++ pages/profile/[username].jsx
@@ -1,89 +1,6 @@
 // import { useRouter } from "next/router";
 import { server } from "../../util/server";
 
-// const Profile = ({ post }) => {
-//   return (
-//     <div className="h-screen">
-//       <h1>Slug: {post.username}</h1>
-//     </div>
-//   );
-// };
-
-// export async function getStaticPaths() {
-//   const res = await fetch(`${server}/api/users/`);
-//   const posts = await res.json();
-//   const paths = posts.result.map((post) => ({
-//     params: { slug: [post.toString()] },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
-// export async function getStaticProps({ params }) {
-//   const { slug } = params;
-
-//   const res = await fetch(`${server}/api/user?Slug=${slug}`);
-//   const data = await res.json();
-//   const post = data[0];
-
-//   return {
-//     props: { post },
-//   };
-// }
-
-// export async function getStaticPaths() {
-//   // Call an external API endpoint to get posts
-//   const res = await fetch(`${server}/api/users/`);
-//   const users = await res.json();
-//   console.log(users);
-//   // Get the paths we want to pre-render based on posts
-//   const paths = users.result.map((user) => ({
-//     params: { slug: { username: user.toString() } },
-//   }));
-
-//   // We'll pre-render only these paths at build time.
-//   // { fallback: false } means other routes should 404.
-//   return { paths, fallback: false };
-// }
-
-// export async function getStaticProps({ slug }) {
-//   // params contains the post `username`.
-//   // If the route is like /posts/1, then params.username is 1
-//   const res = await fetch(`${server}/api/user/${context.params.slug}`);
-//   const post = await res.json();
-
-//   // Pass post data to the page via props
-//   return { props: { user } };
-// }
-
-//
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(`${server}/api/user/phil1`);
-//   const user = await res.json();
-//   console.log(user);
-//   return {
-//     props: {
-//       user,
-//     },
-//   };
-// };
-
-// export const getStaticPaths = async () => {
-//   const res = await fetch(`${server}/api/users/`);
-//   const users = await res.json();
-//   console.log(users);
-//   const usernames = users.result.map((user) => user._username);
-//   const paths = usernames.map((username) => ({ params: { username: username.toString() } }));
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
 export const getStaticPaths = async () => {
   const res = await fetch(`${server}/api/users/`);
   const data = await res.json();
@@ -109,18 +26,18 @@ export const getStaticProps = async (context) => {
   };
 };
 
-const Details = ({ user, tweets }) => {
+const Profile = ({ user, tweets }) => {
   console.log(tweets);
   return (
     <div>
       <p>{user.username}</p>
       <p>{user.fullName}</p>
       <p>{user.email}</p>
-      <p>{tweets[0].text}</p>;
+      {tweets.map((tweet) => (
+        <p>{tweet.text}</p>
+      ))}
     </div>
   );
 };
 
-export default Details;
-
-// export default Profile;
+export default Profile;
