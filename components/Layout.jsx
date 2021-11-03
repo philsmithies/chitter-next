32870602c759d034d313f29fde68dc57eda6bbed
Phil Smithies <philsmithies@gmail.com>
2021-11-03 06:28:51 +0100

    sidescrolling fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <div className="bg-black h-screen w-screen">
+      <div className="bg-black h-screen">
         <div className="flex h-full bg-white justify-evenly">
           <SideBar />
           <div className="flex-1">{children}</div>
