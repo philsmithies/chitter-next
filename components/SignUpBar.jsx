b4cbc7a81ec571a87fe46d57027825dd8b634e73
Phil Smithies <philsmithies@gmail.com>
2021-11-04 05:47:23 +0100

    conditional render

--- components/SignUpBar.jsx
+++ components/SignUpBar.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 const SignUpBar = () => {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  const goToProfile = () => {
+    router.push(`/profile/${session.user.username}`);
+  };
+
   return (
     <>
       <div className="flex bg-white flex-col pt-5 pl-5 pr-5 w-3/12">
-        <a href="/signup">
+        {!session ? (
+          <a href="/signup">
+            <div className="flex flex-col p-4 rounded-lg border-2">
+              <h1 className="font-extrabold">New To Chitter?</h1>
+              <p>Sign up now to get your own personalized timeline!</p>
+              <button className="border-2 flex justify-center mt-3 pt-3 pb-3 rounded-full bg-yellow-400 hover:bg-yellow-500 hover:text-white w-18 font-medium">
+                Sign Up
+              </button>
+            </div>
+          </a>
+        ) : (
           <div className="flex flex-col p-4 rounded-lg border-2">
-            <h1 className="font-extrabold">New To Chitter?</h1>
-            <p>Sign up now to get your own personalized timeline!</p>
-            <button className="border-2 flex justify-center mt-3 pt-3 pb-3 rounded-full bg-yellow-400 hover:bg-yellow-500 hover:text-white w-18 font-medium">
-              Sign Up
+            <h1 className="font-extrabold m-auto">
+              Hello, {session.user.username}
+            </h1>
+            <button
+              className="w-24 h-10 items-center border-2 flex mx-auto justify-center mt-3 pt-3 pb-3 rounded-full bg-yellow-400 hover:bg-yellow-500 hover:text-white font-medium"
+              onClick={goToProfile}
+            >
+              Profile
             </button>
           </div>
-        </a>
+        )}
+
         <p className="mt-3 text-gray-600">
           This is a project by
           <a
