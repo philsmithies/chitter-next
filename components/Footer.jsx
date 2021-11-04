a462164b550ace76881e2e3fdc5dd064f8899fcf
Phil Smithies <philsmithies@gmail.com>
2021-11-04 05:32:00 +0100

    fixed footer width

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,15 +2,15 @@ import Composer from "../public/assets/composer-icon.svg";
 
 const Footer = () => {
   return (
-    <div className="relative ">
-      <div className="fill-current text-yellow-300 p-10">
+    <div className="relative">
+      {/* <div className="fill-current text-yellow-300 p-10">
         <button>
           <Composer className="w-12 fixed bottom-24 right-52 border-2 rounded-full bg-white" />
         </button>
-      </div>
+      </div> */}
       <div className="pt-2 pb-2 fixed bottom-0 w-full flex flex-col items-center bg-yellow-400 border-t-2 border-gray-900">
-        <div className="flex w-5/12 justify-between">
-          <div className="flex flex-col">
+        <div className="flex">
+          <div className="flex flex-col mr-24">
             <h2 className="font-extrabold">Don't miss what's happening.</h2>
             <p>People on Chitter the first to know.</p>
           </div>
