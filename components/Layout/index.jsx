ed9e687cb1610420afdf56d18e0ef327cd43b404
Phil Smithies <philsmithies@gmail.com>
2021-11-05 06:25:37 +0100

    searchbar set up styling, functionality to add

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -3,6 +3,7 @@ import SignUpBar from "../SignUpBar";
 import SideBar from "../Sidebar";
 import { useSession } from "next-auth/client";
 import styles from "./layout.module.css";
+import SearchBar from "../SearchBar";
 
 const Layout = ({ children }) => {
   const session = useSession();
@@ -11,7 +12,10 @@ const Layout = ({ children }) => {
       <div className="h-screen">
         <div className={styles.layoutWrapper}>
           <SideBar />
-          <div className={styles.childrenWrapper}>{children}</div>
+          <div className={styles.childrenWrapper}>
+            <SearchBar />
+            {children}
+          </div>
           <SignUpBar />
         </div>
       </div>
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <div className="h-16 w-full align-start pl-4 pr-4 flex items-center border-b-2 border-l-2 border-r-2">
+      <input
+        type="text"
+        className=" w-full rounded-full h-8 pl-4 bg-gray-200"
+        placeholder="Search Chitter"
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
